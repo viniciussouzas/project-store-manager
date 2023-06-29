5aacbde2a88891b6f8ce29c54fd94222df73de0a
Vinícius Souza <tradutorvinicius@gmail.com>
2023-06-29 14:53:52 -0300

    Resolve requisito 4, sem testar o middleware

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,9 +1,10 @@
 const route = require('express').Router();
 
 const { productsController } = require('../controllers');
+const validateProductName = require('../middlewares/validateProductName');
 
 route.get('/', productsController.listProducts);
 route.get('/:productId', productsController.listProductsById);
-route.post('/', productsController.createProduct);
+route.post('/', validateProductName, productsController.createProduct);
 
 module.exports = route;
